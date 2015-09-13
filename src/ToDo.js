a3cf5e7255886fd4c442b3489513c8e1060046d6
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-13 20:07:35 +0100

    basic functionality working

--- src/ToDo.js
+++ src/ToDo.js
@@ -3,7 +3,6 @@ function ToDo() {
   this.doneList = [];
 };
 
-
 ToDo.prototype.addItem = function(toDoItem) {
   this.toDoList.push(toDoItem);
 	return this.toDoList;
@@ -16,4 +15,51 @@ ToDo.prototype.tickItem = function(toDoItem) {
   }
   this.doneList.push(toDoItem);
 	return this.doneList;
+};
+
+// app vvvvvvvvvvvvvvvvvv //
+
+toDo = new ToDo();
+
+function addToToDoList(){
+	var newItem = document.getElementById('add-to-to-do-list').value;
+  toDo.addItem(newItem);
+  updateToDoList();
+}
+
+function addToDoneList(){
+  var doneItem = document.getElementById('tick').value;
+  toDo.tickItem(doneItem.replace(' done',''));
+  updateToDoList();
+  updateDoneList();
+}
+
+function updateToDoList(){
+  var toDoLister = '';
+  var item;
+  for (item in toDo.toDoList) {
+      toDoLister
+      +="<li id='to-do-item' value='"
+      + toDo.toDoList[item]
+      +"'>"
+      + toDo.toDoList[item]
+      +"<input type='submit' id='tick' class='done-button' value='"
+      + toDo.toDoList[item]
+      +" done' onclick='addToDoneList()'>"
+      +"</li>"
+      ;
+  }
+	document.getElementById('to-do-list').innerHTML = toDoLister;
+};
+
+function updateDoneList(){
+  var doneLister = '';
+  var item;
+  for (item in toDo.doneList) {
+      doneLister
+      +="<li id='done-item'>"
+      + toDo.doneList[item]
+      +"</li>";
+  }
+	document.getElementById('done-list').innerHTML = doneLister;
 };
